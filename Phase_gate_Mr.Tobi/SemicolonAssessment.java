69e68a9e936969bd2bf7233a81009ac337f94ee7
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-06-05 17:06:25 +0100

    Questionaires for semicolon assessment..

--- Phase_gate_Mr.Tobi/SemicolonAssessment.java
+++ Phase_gate_Mr.Tobi/SemicolonAssessment.java
@@ -28,7 +28,7 @@ public class SemicolonAssessment {
 				case 1: {
 						boolean firstQues = true;
 						while (firstQues = true){
-						int count = 0;
+						int counter = 0;
 						String question1 = "What is the full meaning of OOP?";
 						System.out.printf("%s%n (a) Open Opposite Pathway %n(b) Opposite Pathway%n(c) Object Oriented Programming%n(d) None of the above%n", question1);
 
@@ -39,13 +39,17 @@ public class SemicolonAssessment {
 							array[0] = 0;
 							firstQues = false;
 						}
-						else if (selection != "c" && count != 2){
-						count += 1;
+						else if (selection != "c" && counter != 2){
+						counter += 1;
 						firstQues = true;
 						}
 					}
 					} break;
 				case 2: {
+						int counter = 0;
+						boolean secondQues = true;
+						while (secondQues = true){
+
 						String question2 = "What is the capital of Nigeria?";
 						String a = "(a) Lagos";
 						String b = "(b) Abuja";
@@ -54,27 +58,68 @@ public class SemicolonAssessment {
 						System.out.printf(" %s%n %s%n %s%n %s%n %s%n", question2, a, b, c, d);
 
 						System.out.print("Enter selection: ");
-						char selection = input.next().charAt(0);
-						} break;
+						String selection = input.next();
+						if (selection == "b"){
+							System.out.print("Correct");
+							array[1] = 0;
+							secondQues = false;
+						}
+						else if (selection != "b" && counter != 2){
+						counter += 1;
+						secondQues = true;
+						}
+					}
+					} break;
 				case 3: {
+						int counter = 0;
+						boolean thirdQues = true;
+						while (thirdQues = true){
+						int count = 0;
 						String question3 = "Array is a programming language?";
 						String a = "(a) True";
 						String b = "(b) False";
 						System.out.printf(" %s%n %s%n %s%n", question3, a, b);
 
 						System.out.print("Enter selection: ");
-						char selection = input.next().charAt(0);
-						} break;
+						String selection = input.next();
+						if (selection == "b"){
+							System.out.print("Correct");
+							array[2] = 0;
+							thirdQues = false;
+						}
+						else if (selection != "b" && counter != 2){
+						counter += 1;
+						thirdQues = true;
+						}
+					}
+					} break;
 				case 4: {
+						int counter = 0;
+						boolean fourthQues = true;
+						while (fourthQues = true){
 						String question4 = "School educates people.";
 						String a = "(a) False";
 						String b = "(b) True";
 						System.out.printf(" %s%n %s%n %s%n", question4, a, b);
 
 						System.out.print("Enter selection: ");
-						char selection = input.next().charAt(0);
+						String selection = input.next();
+						if (selection == "b"){
+							System.out.print("Correct");
+							array[3] = 0;
+							fourthQues = false;
+						}
+						else if (selection != "b" && counter != 2){
+						counter += 1;
+						fourthQues = true;
+						}
+						}
 						} break;
 				case 5: {
+						int counter = 0;
+						boolean fifthQues = true;
+						while (fifthQues = true){
+
 						String question5 = "Where is Semicolon located?";
 						String a = "(a) Abuja";
 						String b = "(b) Lagos";
@@ -83,9 +128,22 @@ public class SemicolonAssessment {
 						System.out.printf(" %s%n %s%n %s%n %s%n %s%n", question5, a, b, c, d);
 
 						System.out.print("Enter selection: ");
-						char selection = input.next().charAt(0);
+						String selection = input.next();
+						if (selection == "b"){
+							System.out.print("Correct");
+							array[4] = 0;
+							fifthQues = false;
+						}
+						else if (selection != "b" && counter != 2){
+						counter += 1;
+						fifthQues = true;
+						}
+					}
 						}
 				case 6: {
+						int counter = 0;
+						boolean sixthQues = true;
+						while (sixthQues = true){
 						String question6 = "What is 3 * 3";
 						String a = "(a) 6";
 						String b = "(b) 12";
@@ -94,9 +152,22 @@ public class SemicolonAssessment {
 						System.out.printf(" %s%n %s%n %s%n %s%n %s%n", question6, a, b, c, d);
 
 						System.out.print("Enter selection: ");
-						char selection = input.next().charAt(0);
+						String selection = input.next();
+						if (selection == "c"){
+							System.out.print("Correct");
+							array[5] = 0;
+							sixthQues = false;
 						}
+						else if (selection != "c" && counter != 2){
+						counter += 1;
+						sixthQues = true;
+						}
+					}
+						} break;
 				case 7: {
+						int counter = 0;
+						boolean seventhQues = true;
+						while (seventhQues = true){
 						String question7 = "When is christmas?";
 						String a = "(a) 25 Dec";
 						String b = "(b) 25 Jan";
@@ -105,7 +176,17 @@ public class SemicolonAssessment {
 						System.out.printf(" %s%n %s%n %s%n %s%n %s%n", question7, a, b, c, d);
 
 						System.out.print("Enter selection: ");
-						char selection = input.next().charAt(0);
+						String selection = input.next();
+						if (selection == "b"){
+							System.out.print("Correct");
+							array[5] = 0;
+							seventhQues = false;
+						}
+						else if (selection != "b" && counter != 2){
+						counter += 1;
+						seventhQues = true;
+						}
+					}
 						}
 				case 8: {
 						String question8 = "English is a language";
