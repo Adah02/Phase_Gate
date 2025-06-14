9872e9c02b8a2668d8042c02aad7ff95353137c2
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-06-14 23:50:49 +0100

    Check Out App..

--- PhaseGateOne/Python/check_out_app.py
+++ PhaseGateOne/Python/check_out_app.py
@@ -1,5 +1,4 @@
-def sub_total(quantity_of_product):
-	(price_of_product, quantity_of_product)
+def sub_total(price_of_product, quantity_of_product):
 	total_amount = 0
 	for index in range(len(price_of_product)):
 		total_amount += price_of_product[index] * quantity_of_product[index]
@@ -18,8 +17,42 @@ def tax_to_pay(price_of_product, quantity_of_product):
 	tax_amount = (total / PERCENTAGE) * tax_percentage;
 	return tax_amount;
 
+def payment_amount(tax_to_pay, sub_total, purchase_discount):
+	#To determine the total payment amount by customer
+
+	amount_to_pay = (tax_to_pay, + sub_total, - purchase_discount)
+	return amount_to_pay;
+
+def purchase_discount(price_of_product, quantity_of_product):
+	#To calculate discount for purchase amount.
+	totalAmount = 0
+	for index in range(len(price_of_product)):
+		totalAmount += price_of_product[index] * quantity_of_product[index]
+
+	PERCENTAGE =100
+	discount_percentage = 3.8
+	
+	discount_amount = (totalAmount / PERCENTAGE) * discount_percentage;
+	return discount_amount;
+
+def customers_balance(bill, amount_paid):
+	balance = amount_paid - bill
+
+	return balance;
+
+def store_details():
+	#To hold store details
+	StoreDetails = """
+	SEMICOLON STORES
+	MAIN BRANCH
+	LOCATION: 312, HERBERT MACAULAY WAY, SABO YABA, LAGOS.
+	TEL: 03293828343
+	"""
+	return StoreDetails;
 
 	
+
+
 
 
 available_products = """
@@ -48,17 +81,36 @@ quantity_of_product = []
 
 print(available_products)
 
-purchase = int(input('Enter product to purchase: '))
-purchases.append(products[purchase - 1])
-price_of_product.append(prices[purchase - 1])
 
-product_count = int(input('How many do you want? : '))
-quantity_of_product.append(product_count)
+while True:
+	purchase = int(input('Enter product to purchase: '))
+	purchases.append(products[purchase - 1])
+	price_of_product.append(prices[purchase - 1])
+
+	product_count = int(input('How many do you want? : '))
+	quantity_of_product.append(product_count)
 	
-users_choice = int(input('Would you like to add more? Press \"1\"  > Yes or \"0\" > No:'))
+	users_choice = int(input('Would you like to add more? Press \"1\"  > Yes or \"0\" > No:'))
+	match users_choice:
+		case 0:
+			purchase = False
+			break;
+		case 1:
+			purchase = True
+			break;
+		case _:
+			purchase = True
+			break;
 
 
-print(sub_total(quantity_of_product))
+
+
+print(sub_total(price_of_product, quantity_of_product))
 print(tax_to_pay(price_of_product, quantity_of_product))
+
+amount_paid = input('How much did the customer pay: ')
+print(payment_amount(tax_to_pay, sub_total, purchase_discount))
+
+
 
 
