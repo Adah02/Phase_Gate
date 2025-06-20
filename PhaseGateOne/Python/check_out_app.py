f7ba49fa13ae38f84cf1f342831b0435cb267ec4
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-06-20 14:51:15 +0100

    Check Out App

--- PhaseGateOne/Python/check_out_app.py
+++ PhaseGateOne/Python/check_out_app.py
@@ -1,3 +1,4 @@
+
 def sub_total(price_of_product, quantity_of_product):
 	total_amount = 0
 	for index in range(len(price_of_product)):
@@ -5,7 +6,8 @@ def sub_total(price_of_product, quantity_of_product):
 
 	return total_amount;
 
-def tax_to_pay(price_of_product, quantity_of_product):
+
+def value_added_tax(price_of_product, quantity_of_product):
 	#To calculate the total tax amount to be paid.
 	total = 0
 	for index in range(len(price_of_product)):
@@ -17,10 +19,21 @@ def tax_to_pay(price_of_product, quantity_of_product):
 	tax_amount = (total / PERCENTAGE) * tax_percentage;
 	return tax_amount;
 
-def payment_amount(tax_to_pay, sub_total, purchase_discount):
+def payment_amount(price_of_product, quantity_of_product):
 	#To determine the total payment amount by customer
+	
+	total_amount = 0
+	for index in range(len(price_of_product)):
+		total_amount += price_of_product[index] * quantity_of_product[index]
 
-	amount_to_pay = (tax_to_pay, + sub_total, - purchase_discount)
+	PERCENTAGE =100
+	discount_percentage = 3.8
+	tax_percentage = 7.5
+
+	discount_amount = (total_amount / PERCENTAGE) * discount_percentage;
+	tax_amount = (total_amount / PERCENTAGE) * tax_percentage;
+
+	amount_to_pay = (tax_amount + total_amount) - discount_amount
 	return amount_to_pay;
 
 def purchase_discount(price_of_product, quantity_of_product):
@@ -36,81 +49,105 @@ def purchase_discount(price_of_product, quantity_of_product):
 	return discount_amount;
 
 def customers_balance(bill, amount_paid):
-	balance = amount_paid - bill
+
+	balance = (amount_paid - bill)
 
 	return balance;
 
+def header():
+	headings = "	ITEM \t QTY \t PRICE \t TOTAL"
+	
+	return headings
+
+def lines():
+	#Printing lines in receipt
+	line = "=" * 55
+	
+	return line;
+
+
 def store_details():
 	#To hold store details
-	StoreDetails = """
-	SEMICOLON STORES
-	MAIN BRANCH
-	LOCATION: 312, HERBERT MACAULAY WAY, SABO YABA, LAGOS.
-	TEL: 03293828343
-	"""
+	StoreDetails = """SEMICOLON STORES 
+MAIN BRANCH
+LOCATION: 312, HERBERT MACAULAY WAY, SABO YABA, LAGOS.
+TEL: 03293828343
+"""
 	return StoreDetails;
 
-	
 
-
-
-
-available_products = """
-===================================
-    Welcome to Semicolon Stores
-===================================
-    press the respective number
-        to purchase product.
-===================================
-             Press:-
-   1. Rice         2. Ice-Scream
-   3. Bread        4. Yogurt
-   5. Pringles     6. Vegetable-Oil
-   7. Eggs         8. Mash-mallow
-   9. Sadin        10. Table-water
-  11. Peak Milk    12. Snackers
-===================================
-"""
-
-products = ["Rice", "Ice-Scream", "Bread", "Yogurt", "Pringles", "Vegetable-Oil", "Eggs", "Mash-mallow", "Sadin", "Table-water", "Peak Milk", "Snackers"]
-prices = [40000, 2500, 1600, 4500, 3750, 6000, 1500, 3000, 1000, 1900, 7400, 2500]
-
-purchases = []
+products = []
 price_of_product = []
-quantity_of_product = []
+quantity_of_product = []	
 
-print(available_products)
+customers_name = str(input("Enter customer's name: "))
 
+trade = True
+while trade == True:
+	product = input('\nEnter name of product: ')
+	products.append(product )
 
-while True:
-	purchase = int(input('Enter product to purchase: '))
-	purchases.append(products[purchase - 1])
-	price_of_product.append(prices[purchase - 1])
+	price = float(input("\nEnter price of product: "))
+	price_of_product.append(price)
 
-	product_count = int(input('How many do you want? : '))
+	product_count = int(input('\nHow many did the customer buy? : '))
 	quantity_of_product.append(product_count)
 	
-	users_choice = int(input('Would you like to add more? Press \"1\"  > Yes or \"0\" > No:'))
-	match users_choice:
-		case 0:
-			purchase = False
-			break;
-		case 1:
-			purchase = True
-			break;
-		case _:
-			purchase = True
-			break;
+	users_choice = int(input('\nWould you like to add more? \nPress \"1\" > Yes or \"0\" > No: '))
+
+	if users_choice == 1:
+		trade = True
+	else:
+		trade = False
 
 
 
 
-print(sub_total(price_of_product, quantity_of_product))
-print(tax_to_pay(price_of_product, quantity_of_product))
+cashiers_name = str(input("\n Enter cashier's name: "))
+bill = float(payment_amount(price_of_product, quantity_of_product))
 
-amount_paid = input('How much did the customer pay: ')
-print(payment_amount(tax_to_pay, sub_total, purchase_discount))
+product_total_price = []
+for index in range(len(price_of_product)):
+	product_total_price.append(price_of_product[index] * quantity_of_product[index])
 
+
+print(lines())
+print(store_details())
+
+print("Customer's name: ",customers_name)
+print(f"Cashier's name: {cashiers_name} \n{lines()}")
+
+print(f'{header()} \n{lines()}')
+
+for item in range(len(products)):
+	print(f'\t {products[item]}  \t {quantity_of_product[item]:} \t {price_of_product[item]:.2f} \t{product_total_price[item]:.2f}')
+
+print(lines())
+
+print(f" \t Sub-total: {sub_total(price_of_product, quantity_of_product):.2f} \n \t Discount: { purchase_discount(price_of_product, quantity_of_product):.2f}")
+print(f" \t VAX @7.5% : {value_added_tax(price_of_product, quantity_of_product):.2f}\n{lines()}") 
+print(f"\t Bill Total: {payment_amount(price_of_product, quantity_of_product):.2f} \n{lines()}")
+print(f" THIS IS NOT A RECEIPT, KINDLY PAY {payment_amount(price_of_product, quantity_of_product):.2f} \n{lines()}")
+
+amount_paid = float(input('\nHow much did the customer pay: '))
+
+balance = customers_balance(bill, amount_paid)
+
+print(f'{lines()} \n{header()} \n{lines()}')
+
+for item in range(len(products)):
+	print(f'\t {products[item]}  \t {quantity_of_product[item]:} \t {price_of_product[item]:.2f} \t{product_total_price[item]:.2f}')
+
+print(lines())
+
+print(f" \t Sub-total: {sub_total(price_of_product, quantity_of_product):.2f} \n \t Discount: { purchase_discount(price_of_product, quantity_of_product):.2f}")
+print(f" \t VAX @7.5% : {value_added_tax(price_of_product, quantity_of_product):.2f}\n{lines()}") 
+print(f"\t Bill Total: {payment_amount(price_of_product, quantity_of_product):.2f} \n  \tAmount Paid:  {amount_paid:.2f} \n \tBalance: {balance:.2f}\n")
+
+if balance < 0:
+	print(f"\tYou still owe {abs(balance):.2f}, Please pay up")
+
+print(f"{lines()}\n \t THANKS FOR YOUR PATRONAGE \n{lines()}")
 
 
 
