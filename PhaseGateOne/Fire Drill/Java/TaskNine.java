5e6d4dac6cb741bdcd1021c446af9b1dff21ba6f
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-06-24 02:41:11 +0100

    Task..

--- PhaseGateOne/Fire Drill/Java/TaskNine.java
+++ PhaseGateOne/Fire Drill/Java/TaskNine.java
@@ -1,16 +1,16 @@
-public class TaskFive {
+public class TaskNine {
 
 	public static void main(String[] args){
+
+	int sum = 0;
 
 	for (int index = 1; index <= 10; index++){
 		if (index % 4 == 0){
 			for (int count = 1; count <= 5; count++){
-			System.out.print(index);
+			sum += Math.pow(index, count);
 			}
-		System.out.print(" ");
 		}
 	}
-
+	System.out.printf("%.2f",Math.sqrt(sum));
 	}
-
 }
